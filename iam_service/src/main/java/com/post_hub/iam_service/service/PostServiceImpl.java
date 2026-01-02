f838d5786bff77ba78f3abfbc8d5c8fd86e907fa
uladzislau_maiseyeu <vladislavmoiseev9@gmail.com>
2026-01-02 18:01:21 +0300

    3.1

---
+++ iam_service/src/main/java/com/post_hub/iam_service/service/PostServiceImpl.java
@@ -0,0 +1,17 @@
+package com.post_hub.iam_service.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private final List<String> posts = new ArrayList<>();
+
+    @Override
+    public void createPost(String postContent) {
+        posts.add(postContent);
+    }
+}
